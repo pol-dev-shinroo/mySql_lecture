b82f074dea77f3bc3c5c2d2af5816fbdec2f941c
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-03-19 04:12:25 +0900

    substring

--- stringFnPrac/book_data.sql
+++ stringFnPrac/book_data.sql
@@ -1,4 +1,10 @@
 /* 1. concat + concat_ws */
-SELECT *	FROM books;
+/* SELECT *	FROM books;
 SELECT CONCAT(author_fname, " ", author_lname) as author_fullname from books;
-SELECT CONCAT_WS(" ",author_fname, author_lname) as author_fullname from books;
+SELECT CONCAT_WS(" ",author_fname, author_lname) as author_fullname from books; */
+
+/* substring */
+/* SELECT SUBSTRING("Hello world", -3) */
+/* SELECT SUBSTRING(title, 1, 15) as title_sub from books; */
+/* SELECT SUBSTR(title, 1, 15) as title_sub from books; */
+/* SELECT SUBSTR(title, 1, 1) as initials from books; */
