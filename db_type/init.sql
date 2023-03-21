1614a1bb844e768b98520f941c0bb758f71cd74d
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-03-22 03:33:47 +0900

    decimals, float, double

--- db_type/init.sql
+++ db_type/init.sql
@@ -11,3 +11,12 @@
 /* 2. int, tinyint, bigint */
 /* the range differs among them, check the documentation */
 /* create table parent (children tinyint unsigned); */
+/* ****************************** */
+/* 3. Decimals  */
+/* drop table parent;
+ create table parent (children decimal(5, 2)); */
+/* insert into
+ parent(children)
+ values
+ (123123.12123); */
+/* show warnings; */
