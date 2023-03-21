1d68b167af74ae9166693be35058c49b160db79d
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-03-21 17:13:25 +0900

    refining functions exercise complete

--- refining_selections/init.sql
+++ refining_selections/init.sql
@@ -68,3 +68,85 @@
  books
  where
  author_fname like '_____' */
+/* *************************************************** */
+/* Exercise */
+/* select
+ title
+ from
+ books
+ where
+ title like '%stories%' */
+/* select
+ title,
+ pages
+ from
+ books
+ order by
+ 2 desc
+ limit
+ 1; */
+/* select
+ concat(title, ' - ', released_year) as summary
+ from
+ books
+ where
+ title is not null
+ order by
+ released_year desc
+ limit
+ 3; */
+/* select
+ title,
+ author_lname
+ from
+ books
+ where
+ author_lname like '% %' */
+/* select
+ title,
+ released_year,
+ stock_quantity
+ from
+ books
+ where
+ title is not null
+ order by
+ 3
+ limit
+ 3 */
+/* select
+ title,
+ author_lname
+ from
+ books
+ order by
+ 2,
+ 1; */
+/* select
+ concat(
+ upper('my favorite author is '),
+ upper(author_lname)
+ ) as yell
+ from
+ books
+ order by
+ 1 */
+/* select
+ concat (title, ' - ', released_year) as summary
+ from
+ books
+ where
+ title is not null
+ order by
+ released_year desc
+ limit
+ 3 */
+select
+ title,
+ pages
+from
+ books
+order by
+ length(title) desc
+limit
+ 1
