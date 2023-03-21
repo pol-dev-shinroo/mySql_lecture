44ea054ae97616a686ee47c5714e0621463832d8
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-03-21 17:25:29 +0900

    count

---
+++ aggregate_fn/init.sql
@@ -0,0 +1,25 @@
+/* ************************************************* */
+/* 1. count */
+/* select
+ count(*)
+ from
+ books; */
+/* select
+ count(title)
+ from
+ books; */
+/* select
+ count(author_fname)
+ from
+ books;
+ 
+ select
+ count(distinct author_fname)
+ from
+ books; */
+/* select
+ count(*)
+ from
+ books
+ where
+ title like '%the%' */
