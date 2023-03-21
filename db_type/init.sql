2000e79249b45a02ee687568cbb5e344f8ab08bd
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-03-22 03:18:38 +0900

    char vs varchar

---
+++ db_type/init.sql
@@ -0,0 +1,9 @@
+/* ****************************** */
+/* 1. char vs varchar */
+/* create table states (abbr char(2)); */
+/* insert into
+ states (abbr)
+ values
+ ("C"); */
+/* CHAR is faster for fixed length text */
+/* Otherwise use VARCHAR */
