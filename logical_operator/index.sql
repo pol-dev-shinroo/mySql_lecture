c7c11cd54da8550c5b978117f1efd16c5c3564c9
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-03-22 15:20:03 +0900

    logical operators

---
+++ logical_operator/index.sql
@@ -0,0 +1,41 @@
+/* ****************************** */
+/* 1. logical operators */
+/* SELECT
+ *
+ FROM
+ books
+ WHERE
+ released_year != 2017; */
+/* SELECT
+ *
+ FROM
+ books
+ WHERE
+ released_year > 2005;
+ 
+ SELECT
+ *
+ FROM
+ books
+ WHERE
+ pages > 500; */
+/* SELECT
+ *
+ FROM
+ books
+ WHERE
+ pages < 200;
+ 
+ SELECT
+ *
+ FROM
+ books
+ WHERE
+ released_year < 2000;
+ 
+ SELECT
+ *
+ FROM
+ books
+ WHERE
+ released_year <= 1985; */
